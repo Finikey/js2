500e4d21f5f7a03f654e6dd6bead78ce031e6851
finikey <finikeyshow@gmail.com>
2023-09-07 16:20:00 +0300

    new

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+// let a = 'text';
+// let b = 345;
+
+// console.log('te')
+// console.log('a');
+// console.log('b');
+// console.log(2458)
+
+// let x = 100
+// let y = 100
+// console.log(y * x)
+// console.log('---------')
+
+// alert('gfdgdfgd' + a);
+// // let what = prompt ('что?')
+// // console.log(what)
+// // Number(what);
+// // console.log(Number(what))
+// let sure = confirm('уверeн')
+// console.log(sure)
+
+// let year = 2023
+
+// let a =prompt ('название города')
+// let b =prompt ('год образования города')
+// let c =prompt ('население')
+
+// console.log()
+
+let cityName = prompt("Введите название города");
+let yearformat = Number(prompt("Введите год образования города"));
+let population = Number(prompt("Введите население города"));
+
+let agecity = 2023 - yearformat;
+
+console.log(`Городу ${cityName} исполнилось ${agecity} лет с момента его образования. Население - ${population} человек`);
+
+
